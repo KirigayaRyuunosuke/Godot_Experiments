69cccfd657f00f97734e72d311ee5ac73c75240c
Franek <michalakfranciszek2005@gmail.com>
2025-04-08 19:53:08 +0200

    Random zones are generated, sorted and partialy removed

--- procedural_cave/nodes/base/base.gd
+++ procedural_cave/nodes/base/base.gd
@@ -21,77 +21,116 @@ func _ready() -> void:
 	down.global_position.y = rows * 100
 	right.global_position.x = cols * 100
 
-	_drawMap(_mapGenerator(rows,cols,halls))
-	player.global_position = (playerStartingPosition*100) + Vector2(35,35)
+	_drawMap(_mapGenerator(rows,cols))
 
-func _mapGenerator(rows,cols,halls):
+func _mapGenerator(rows,cols):
 	var result = []
-	var points = []
-	points = _randomPointsGenerator(rows,cols,halls)
-	result = _drawHalls(points,rows,cols)
-	result = _drawRoads(points,result)
+	result = _randomPointsGenerator(rows,cols)
+	result = _clear(result)
+	
+	for a in range(result.size()):
+		print(result[a])
+	
 	return result
 
-func _drawHalls(points,rows,cols):
+func _randomPointsGenerator(rangeX,rangeY):
 	var result = []
-	for a in range(rows):
+	for a in range(rangeX):
 		result.append([])
-		for i in range(cols):
-			result[a].append(0)
-	for i in range(points.size()):
-		for x in range(3):
-			for y in range(3):
-				result[points[i][0]+y][points[i][1]+x] = 1
+		for i in range(rangeY):
+			result[a].append(randi() % 2)
 	return result
 
-func _randomPointsGenerator(rangeX,rangeY,howMany):
-	var points = []
-	var x = (rangeX-(rangeX%3))/3
-	var y = (rangeY-(rangeY%3))/3
-	var newX = (randi()%x)*3
-	var newY = (randi()%y)*3
-	points.append([newX,newY])
-	var i = 0
-	var tries = 0
-	while i < howMany-1 && tries < 100:
-		tries += 1
-		newX = (randi()%x)*3
-		newY = (randi()%y)*3
-		if _checkAvailability(points,[newX,newY]):
-			points.append([newX,newY])
-			i += 1
-	return points
+func _clear(array):
+	var result = array
+	
+	result = _mapZones(result)
+	result = _connectZones(result)
+	result = _remapZones(result)
+	
+	var zonesCounter = []
+	for i in range(_findBiggestZone(result)+1):
+		zonesCounter.append(0)
+	for a in range(result.size()):
+		for i in range(result[a].size()):
+			if result[a][i] > 0:
+				zonesCounter[result[a][i]] += 1
+	for i in range(zonesCounter.size()):
+		if zonesCounter[i] < 10 || zonesCounter[i] > 30:
+			result = _removeZone(result,i)
+	result = _remapZones(result)
+	return result
 
-func _checkAvailability(points,newPoint):
-	var distanceX
-	var distanceY
-	var minimalDistanceBetween = 4
-	var maximumDistanceBetween = 100
-	for i in range(points.size()):
-		distanceX = points[i][0] - newPoint[0]
-		distanceY = points[i][1] - newPoint[1]
-		if distanceX < 0: distanceX *= -1
-		if distanceY < 0: distanceY *= -1
-		if distanceX < minimalDistanceBetween && distanceY < minimalDistanceBetween:
-			return 0
-	distanceX = points[points.size()-1][0] - newPoint[0]
-	distanceY = points[points.size()-1][1] - newPoint[1]
-	if distanceX < 0: distanceX *= -1
-	if distanceY < 0: distanceY *= -1
-	if distanceX < maximumDistanceBetween && distanceY < maximumDistanceBetween:
-		return 1
-	return 0
+func _removeZone(array,zone):
+	return _changeValuesInArray(array,zone,0)
 
-func _drawRoads(points,map):
-	var result = map
-	var centerPoints = []
-	for x in range(points.size()):
-		centerPoints.append([])
-		for y in range(points[x].size()):
-			centerPoints[x].append(points[x][y]+1)
-	for i in range(centerPoints.size()-1):
-		result = _drawLine(centerPoints[i],centerPoints[i+1],result)
-	playerStartingPosition = Vector2(centerPoints[0][1],centerPoints[0][0])
+func _findBiggestZone(array):
+	var result = 0
+	for a in range(array.size()):
+		for i in range(array[a].size()):
+			if array[a][i] > result:
+				result = array[a][i]
+	return result
+
+func _mapZones(array):
+	var groups = 1
+	var result = array
+	for a in range(result.size()):
+		for i in range(result[a].size()):
+			if array[a][i] == 1 :
+				if a - 1 >= 0 && result[a-1][i] > 1:				# up
+					result[a][i] = result[a-1][i]
+				elif a + 1 < result.size() && result[a+1][i] > 1:		# down
+					result[a][i] = result[a+1][i]
+				elif  i - 1 >= 0 && result[a][i-1] > 1:				# left
+					result[a][i] = result[a][i-1]
+				elif i + 1 < result[a].size() && result[a][i+1] > 1:	# right
+					result[a][i] = result[a][i+1]
+				else:
+					groups += 1
+					result[a][i] = groups
+	return result
+
+func _connectZones(array):
+	var result = array
+	for a in range(result.size()):
+		for i in range(result[a].size()):
+			if result[a][i] > 0:
+				if a - 1 >= 0 && result[a-1][i] > 1:				# up
+					result = _changeValuesInArray(result,result[a-1][i],result[a][i])
+				if a + 1 < result.size() && result[a+1][i] > 1:		# down
+					result = _changeValuesInArray(result,result[a+1][i],result[a][i])
+				if  i - 1 >= 0 && result[a][i-1] > 1:				# left
+					result = _changeValuesInArray(result,result[a][i-1],result[a][i])
+				if i + 1 < result[a].size() && result[a][i+1] > 1:	# right
+					result = _changeValuesInArray(result,result[a][i+1],result[a][i])
+	return result
+
+func _remapZones(array):
+	var result = array
+	var newValue = []
+	var isIn : bool = 0
+	for a in range(result.size()):
+		for i in range(result[a].size()):
+			isIn = 0
+			if result[a][i] > 0:
+				if newValue.size() == 0:
+					newValue.append(result[a][i])
+				for c in range(newValue.size()):
+					if newValue[c] == result[a][i]:
+						isIn = 1
+				if !isIn:
+					newValue.append(result[a][i])
+	for i in range(newValue.size()):
+		result = _changeValuesInArray(result,newValue[i],i+1)
+	return result
+
+func _changeValuesInArray(array,from,to):
+	var result = array
+	for a in range(result.size()):
+		for i in range(result[a].size()):
+			if result[a][i] == from:
+				result[a][i] = to
 	return result
 
 func _drawLine(pointA,pointB,array):
@@ -120,7 +159,7 @@ func _drawMap(array):
 	for a in range(array.size()):
 		for i in range(array[a].size()):
 			drawingPosition = Vector2(i*100,a*100)
-			if array[a][i] == 1:
+			if array[a][i] > 0:
 				var segment = segmentFloor.instantiate()
 				segment.global_position = drawingPosition
 				mapNode.add_child(segment)
