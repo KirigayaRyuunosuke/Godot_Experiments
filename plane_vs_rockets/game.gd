28fe7080e295875f72b095a22acc3519d1e8c216
Franek <michalakfranciszek2005@gmail.com>
2025-05-07 21:39:03 +0200

    added full SQL integration

--- plane_vs_rockets/game.gd
+++ plane_vs_rockets/game.gd
@@ -13,8 +13,14 @@ var plane
 var main
 
 var points = 0
+var highscore
 
 func end():
+	var data = {
+		"score" : points
+	}
+	database.insert_row("sessions", data)
+	highscore = _getHighScore()
 	points = 0
 	get_tree().change_scene_to_file("res://nodes/menu/end.tscn")
 
@@ -22,7 +28,19 @@ func _ready() -> void:
 	database = SQLite.new()
 	database.path = "res://score.db"
 	database.open_db()
+	if !database.query("SELECT * FROM sessions"):
+		_createTable()
+	highscore = _getHighScore()
 
+func _getHighScore():
+	database.query("SELECT score FROM sessions ORDER BY score DESC LIMIT 1")
+	if database.query_result.size() :
+		return database.query_result[0]["score"]
 
-func _process(_delta: float) -> void:
-	pass
+func _createTable():
+	database.create_table("sessions",
+		{
+			"id": {"data_type":"int","primary_key":true,"not_null":true, "auto_increment":true},
+			"score": {"data_type":"int","not_null":true}
+		}
+	)
--- plane_vs_rockets/nodes/menu/menu.gd
+++ plane_vs_rockets/nodes/menu/menu.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
+@onready var high_score: Label = $CanvasLayer/HighScore
+
 var game = load("res://nodes/main.tscn")
 
+
+func _physics_process(_delta: float) -> void:
+	high_score.text = "High score: " + str(Game.highscore)
+	
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_packed(game)
